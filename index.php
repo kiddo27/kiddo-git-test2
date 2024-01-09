f9c460cbb2b3af02a92e851589b0ad21decc032f
kiddo27 <braveninja227@gmail.com>
2024-01-09 14:20:10 +0700

    menambahkan paragraf 2

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 <body>
 	<h1> hello world</h1>
 	<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum sed modi, quae tenetur aut aspernatur, dolore maxime assumenda ullam est et, autem porro rerum, mollitia accusantium facilis culpa voluptatibus voluptatem?</p>
+	<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis id quas hic voluptas, dolorem delectus, placeat. Autem iure qui consequatur voluptates ipsa quis error adipisci, dolores commodi. Dolorem, numquam, consequuntur?</p>
 </body>
 </html>
